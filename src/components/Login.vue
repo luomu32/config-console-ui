7f36415aa69a62d6d953268f1ea30fae60880dff
luomu32 <luomu32@gmail.com>
2018-12-18 20:43:54 +0800

    set layout to center

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,50 +1,50 @@
 <template>
-    
-        <div class="login">
+  <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="" :bordered="true">
+      <Card icon="log-in" title :bordered="true">
         <div class="form-con">
-
-        <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
-    <FormItem prop="userName">
-      <Input v-model="form.userName" placeholder="请输入用户名">
-        <span slot="prepend">
-          <Icon :size="16" type="ios-person"></Icon>
-        </span>
-      </Input>
-    </FormItem>
-    <FormItem prop="password">
-      <Input type="password" v-model="form.password" placeholder="请输入密码">
-        <span slot="prepend">
-          <Icon :size="14" type="md-lock"></Icon>
-        </span>
-      </Input>
-    </FormItem>
-    <FormItem>
-      <Button @click="handleSubmit" type="primary" long>登录</Button>
-    </FormItem>
-  </Form>
-          </div>
+          <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
+            <FormItem prop="name">
+              <Input v-model="form.name" placeholder="请输入用户名">
+                <span slot="prepend">
+                  <Icon :size="16" type="ios-person"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem prop="password">
+              <Input type="password" v-model="form.password" placeholder="请输入密码">
+                <span slot="prepend">
+                  <Icon :size="14" type="md-lock"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem>
+              <Button @click="handleSubmit" type="primary" long>登录</Button>
+            </FormItem>
+          </Form>
+        </div>
       </Card>
     </div>
   </div>
-    
 </template>
 <style scoped lang="less">
 .login {
   width: 100%;
   height: 100%;
-  //   background-image: url("../../assets/images/login-bg.jpg");
   background-size: cover;
   background-position: center;
   position: relative;
+
   &-con {
     position: absolute;
-    // right: 160px;
-    margin: 0 auto;
+    margin: auto;
+    left: 0px;
+    right: 0px;
+    margin: auto;
     top: 50%;
-    transform: translateY(-60%);
+    transform: translateY(-50%);
     width: 300px;
+
     &-header {
       font-size: 16px;
       font-weight: 300;
@@ -69,11 +69,11 @@ export default {
   data() {
     return {
       form: {
-        userName: "",
+        name: "",
         password: ""
       },
       rules: {
-        userName: [{ required: true, message: "用户名不能为空" }],
+        name: [{ required: true, message: "用户名不能为空" }],
         password: [{ required: true, message: "密码不能为空" }]
       }
       //   loginBtnFlag: false
@@ -83,14 +83,14 @@ export default {
     handleSubmit: function() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.$http
-            .postForm("auth", {
-              name: this.form.userName,
-              password: this.form.password
-            })
-            .then(() => {
-              Cookies.set("token", this.form.userName);
-              this.$router.replace({ name: "home" });
+          this.$ajax
+            .post("auth")
+            .form(this.form)
+            .send({
+              success: () => {
+                Cookies.set("token", this.form.userName);
+                this.$router.replace({ name: "home" });
+              }
             });
         }
       });
