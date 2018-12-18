3d9b41ac249dccf02fcc7307935d1910982de955
luomu32 <luomu32@gmail.com>
2018-12-18 20:43:41 +0800

    change http request style

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,49 +1,40 @@
 <template>
-    <Layout style="height: 100%" class="main">
-        <Sider hide-trigger :width="256"  class="left-sider" :style="{overflow: 'hidden'}">
-            <div class="logo-con">
-                    配置中心管理平台
-            </div>
-            <Menu theme="dark" :active-name="activeMenuName" width="auto">
-                <MenuItem name="server" to="/server">
-                    <Icon type="md-filing"></Icon>
-                            资源库管理
-                </MenuItem>
-                <MenuItem name="application" to="/application">
-                    <Icon type="md-apps"></Icon>
-                            应用管理
-                </MenuItem>
-                <MenuItem name="config" to="/config">
-                    <Icon type="md-apps"></Icon>
-                            配置管理
-                </MenuItem>
-                <MenuItem name="3">
-                    <Icon type="md-key"></Icon>
-                            加密设置
-                </MenuItem>
-                <MenuItem name="user" to="/user">
-                    <Icon type="md-person"></Icon>
-                            用户管理
-                </MenuItem>
-                <MenuItem name="role" to="/role">
-                    <Icon type="md-people"></Icon>
-                            角色管理
-                </MenuItem>
-                <MenuItem name="log" to="/log">
-                    <Icon type="ios-archive"></Icon>
-                            操作日志
-                </MenuItem>
-            </Menu>
-        </Sider>
-        <Layout>
-            <Header class="header-con">
-                <Button @click="loginOut" icon="md-log-out">退出</Button>
-            </Header>
-            <Content class="content-wrapper">
-                <router-view/>
-            </Content>
-        </Layout>
+  <Layout style="height: 100%" class="main">
+    <Sider hide-trigger :width="256" class="left-sider" :style="{overflow: 'hidden'}">
+      <div class="logo-con">配置中心管理平台</div>
+      <Menu theme="dark" :active-name="activeMenuName" width="auto">
+        <MenuItem name="server" to="/server">
+          <Icon type="md-filing"></Icon>资源库管理
+        </MenuItem>
+        <MenuItem name="application" to="/application">
+          <Icon type="md-apps"></Icon>应用管理
+        </MenuItem>
+        <MenuItem name="config" to="/config">
+          <Icon type="md-apps"></Icon>配置管理
+        </MenuItem>
+        <MenuItem name="3">
+          <Icon type="md-key"></Icon>加密设置
+        </MenuItem>
+        <MenuItem name="user" to="/user">
+          <Icon type="md-person"></Icon>用户管理
+        </MenuItem>
+        <MenuItem name="role" to="/role">
+          <Icon type="md-people"></Icon>角色管理
+        </MenuItem>
+        <MenuItem name="log" to="/log">
+          <Icon type="ios-archive"></Icon>操作日志
+        </MenuItem>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header class="header-con">
+        <Button @click="loginOut" icon="md-log-out">退出</Button>
+      </Header>
+      <Content class="content-wrapper">
+        <router-view/>
+      </Content>
     </Layout>
+  </Layout>
 </template>
 <style lang="less">
 /* .container{
@@ -105,9 +96,11 @@ export default {
   },
   methods: {
     loginOut: function() {
-      this.$http.post("/sing-out").then(() => {
-        cookie.remove("token");
-        this.$router.replace({ name: "login" });
+      this.$ajax.post("/sing-out").send({
+        success: () => {
+          cookie.remove("token");
+          this.$router.replace({ name: "login" });
+        }
       });
     }
   }
